cc76935513225ed9477b5417c0d578626a1db066
Jepkemoi Sheilah <jepkemoishyllah@gmail.com>
2025-05-07 21:51:12 +0300

    creted while and for loop

--- lib/looping.py
+++ lib/looping.py
@@ -2,12 +2,29 @@
 
 def happy_new_year():
     # code goes here!
-    pass
+    countdown = 10
+    while countdown > 0:
+        print(countdown)
+        countdown -= 1
+    print("Happy New Year!")
 
 def square_integers(int_list):
+    
     # code goes here!
-    pass
+    squared_list = []
+    for i in int_list:
+        squared_list.append(i ** 2)
+    return squared_list
 
 def fizzbuzz():
     # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
+
